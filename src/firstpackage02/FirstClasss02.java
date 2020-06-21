1311b957f156a7c3f8849a2b246a7f8bfd58565d
mschukru <mschukru@gmail.com>
2020-06-21 13:37:37 +0200

    first commit

---
+++ src/firstpackage02/FirstClasss02.java
@@ -0,0 +1,11 @@
+package firstpackage02;
+
+public class FirstClasss02 {
+
+	public static void main(String[] args) {
+		System.out.println("first commit from local repository");
+		System.out.println("second commit from local repository");
+
+	}
+
+}
